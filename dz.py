7eabc312ef776e8c114acd3b2783e69c5d80fc92
Raf-kun <14katosik03@gmail.com>
2024-11-03 16:30:23 +0000

    Что то

--- dz.py
+++ dz.py
@@ -1,4 +1,6 @@
-num1 = int(input("Первая диагональ ромба: "))
-num2 = int(input("Вторая диагональ ромба: "))
-result = (num1 * num2) / 2
-print("Площадь ромба:", result)
+num1 = int(input("Введите первое число:" ))
+num2 = int(input("Введите первое число:" ))
+num3 = int(input("Введите первое число:" ))
+num4 = int(input("Введите первое число:" ))
+result = num1 * num2 * num3 * num4
+print("Резульат", result)
