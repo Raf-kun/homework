11c6d903fe070f9f95ea5bcc33d7671008487b62
Raf-kun <14katosik03@gmail.com>
2024-11-08 16:42:06 +0000

    1-2

--- dz.py
+++ dz.py
@@ -1,6 +1,28 @@
-num1 = int(input("Введите первое число:" ))
-num2 = int(input("Введите первое число:" ))
-num3 = int(input("Введите первое число:" ))
-num4 = int(input("Введите первое число:" ))
-result = num1 * num2 * num3 * num4
-print("Резульат", result)
+num = int(input("Введите число: "))
+match num:
+    case 1:
+        print("Январь.")
+    case 2:
+        print("Февраль.")
+    case 3:
+        print("Март.")
+    case 4:
+        print("Апрель.")
+    case 5:
+        print("Май.")
+    case 6:
+        print("Июнь.")
+    case 7:
+        print("Июль.")
+    case 8:
+        print("Август.")
+    case 9:
+        print("Сентябрь.")
+    case 10:
+        print("Октябрь.")
+    case 11:
+        print("Ноябрь.")
+    case 12:
+        print("Декабрь.")
+    case _:
+        print("Нет такого месяца.")
