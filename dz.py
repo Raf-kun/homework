d288396f7d4614f292cb3c1fd5042485395e134c
Raf-kun <14katosik03@gmail.com>
2024-11-08 16:52:26 +0000

    МНЕ НРАВИТСЯ ELSE

--- dz.py
+++ dz.py
@@ -1,28 +1,7 @@
 num = int(input("Введите число: "))
-match num:
-    case 1:
-        print("Январь.")
-    case 2:
-        print("Февраль.")
-    case 3:
-        print("Март.")
-    case 4:
-        print("Апрель.")
-    case 5:
-        print("Май.")
-    case 6:
-        print("Июнь.")
-    case 7:
-        print("Июль.")
-    case 8:
-        print("Август.")
-    case 9:
-        print("Сентябрь.")
-    case 10:
-        print("Октябрь.")
-    case 11:
-        print("Ноябрь.")
-    case 12:
-        print("Декабрь.")
-    case _:
-        print("Нет такого месяца.")
+if num == 0:
+    print("Number is equal to zero.")
+elif num >= 0:
+    print("Number is positive!")
+else:
+    print("Number is negative.")
