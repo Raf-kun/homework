d2b96ea2b3c1aeaaa47f9cd5840a315e8a9e8317
Raf-kun <14katosik03@gmail.com>
2024-11-29 21:42:25 +0300

    Update Dz10.1.py
    Дз 10 номер 1

--- Dz10.1.py
+++ Dz10.1.py
@@ -1,1 +1,13 @@
+num1 = int(input("Введите число:"))
+num2 = int(input("Введите второе число:"))
+list_of_numbers = []
 
+for i in range(num1, num2 + 1):
+    if i == 1:
+        list_of_numbers append(i)
+    else:
+        for i in range(2, i):
+            if i % j == 0:
+                list_of_numbers append(i)
+
+print(set(list_of_numbers append))
