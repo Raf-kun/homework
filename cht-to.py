ecfcec20bbf8bd8173749b4ed2f1c4d79203db87
Raf-kun <14katosik03@gmail.com>
2024-12-11 20:57:11 +0300

    Update cht-to.py

--- cht-to.py
+++ cht-to.py
@@ -1,1 +1,23 @@
+import time
+
+print("Подойти к раковине")
+time.sleep(2)
+print("Включаем воду")
+time.sleep(3)
+print("Складываем посуду в раковину")
+time.sleep(4)
+print("Процесс мытья посуды")
+time.sleep(5)
+posuda = int(input("Сколько там посуды?: "))
+i = 0
+while i <= posuda:
+    print("Намыливаем губку...")
+    time.sleep(5)
+    print("Моем поусуду.... моем...")
+    time.sleep(7)
+    i += 1
+print("Протираем помытую посуду...")
+time.sleep(5)
+print("Убираем посуду! Юху! Мы все сделали, можем идти спать!")
+time.sleep(5)
 
