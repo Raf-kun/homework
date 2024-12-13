a3c89fe03e2fe91e75856877c876485854bdab27
Raf-kun <14katosik03@gmail.com>
2024-12-13 19:34:46 +0300

    Update Kiri.py
    Задача 1

--- Kiri.py
+++ Kiri.py
@@ -1,1 +1,6 @@
-
+num = int(input("Введите двузначное число: "))
+desyatki = num // 10
+edinitsi = num % 10
+summa = desyatki + edinitsi
+print(f"В числе {num},\n десятки - {desyatki},\n"
+                f"единицы - {edinitsi},\n сумма - {summa}")
